9de3743bb0541f171c634b6b1a386d3a3a0a5f01
Stefan Foulis <stefan@foulis.ch>
2015-05-26 07:59:08 +0200

    manual docker talk notes

---
+++ 01_manual_docker/_notes.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+docker ps
+
+docker run -it ubuntu:14.04 /bin/bash
+
+root@98f74cda9c12:/# apt-get update
+root@98f74cda9c12:/# apt-get install cowsay
+root@98f74cda9c12:/# rm -rf /var/lib/apt/lists/*
+root@98f74cda9c12:/# echo '/usr/games/cowsay "This is my docker image. There are many like it. But this one is mine."' >> /root/.bashrc
+
+
+docker commit -m "add cowsay" 98f74cda9c12 stefanfoulis/cowsay:latest
+
+docker run -it --rm stefanfoulis/cowsay:latest /bin/bash
+ _________________________________________
+/ This is my docker image. There are many \
+\ like it. But this one is mine.          /
+ -----------------------------------------
+        \   ^__^
+         \  (oo)\_______
+            (__)\       )\/\
+                ||----w |
+                ||     ||
+root@98f74cda9c12:/#
+
+docker push stefanfoulis/cowsay:latest
