2921a7515ca7128168a32b38e11ff600459cfb09
Stefan Foulis <stefan@foulis.ch>
2015-05-26 16:12:32 +0200

    rename image

--- 03_django/_notes.sh
+++ 03_django/_notes.sh
@@ -1,14 +1,14 @@
 # build the image
-docker build -t talk-1 .
+docker build -t mydjangoimage --no-cache .
 
 # do startproject
-docker run -it --rm --user "$(id -u):$(id -g)" -v "$PWD":/usr/src/app -w /usr/src/app talk-1 django-admin.py startproject mysite .
+docker run -it --rm --user "$(id -u):$(id -g)" -v "$PWD":/usr/src/app -w /usr/src/app mydjangoimage django-admin.py startproject mysite .
 
 # build again with new sourcecode
-docker build -t talk-1a .
+docker build -t mydjangoimage .
 
 # runserver
-docker run -it --rm -p 8000:8000 talk-1a
+docker run -it --rm -p 8000:8000 mydjangoimage
 
 # runserver with mounted code
-docker run -it --rm -p 8000:8000 -v "$PWD":/usr/src/app talk-1a
+docker run -it --rm -p 8000:8000 -v "$PWD":/usr/src/app mydjangoimage
