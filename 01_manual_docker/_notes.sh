15a158027ea52a95681819ceb41f77d4fbe17542
Stefan Foulis <stefan@foulis.ch>
2015-05-26 18:17:22 +0200

    cowsay enhancement

--- 01_manual_docker/_notes.sh
+++ 01_manual_docker/_notes.sh
@@ -7,10 +7,13 @@ docker run -it ubuntu:14.04 /bin/bash
 root@98f74cda9c12:/# apt-get update
 root@98f74cda9c12:/# apt-get install cowsay
 root@98f74cda9c12:/# rm -rf /var/lib/apt/lists/*
-root@98f74cda9c12:/# echo '/usr/games/cowsay "This is my docker image. There are many like it. But this one is mine."' >> /root/.bashrc
+root@98f74cda9c12:/# echo '/usr/games/cowsay "This is my docker image. There are many like it. But this one is mine. Container $HOSTNAME"' >> /root/.bashrc
 
+docker history dffaa80900e5
 
 docker commit -m "add cowsay" 98f74cda9c12 stefanfoulis/cowsay:latest
+
+docker history stefanfoulis/cowsay:latest
 
 docker run -it --rm stefanfoulis/cowsay:latest /bin/bash
  _________________________________________
